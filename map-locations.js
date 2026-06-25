cc08ff6cc87dc2644f03d076b97cb5d263e6b3d4
ScrollStopAdmin <admin@scrollstop.au>
2026-06-25 12:38:23 +1000

    Add files via upload

---
+++ map-locations.js
@@ -0,0 +1,123 @@
+window.customMapLocations = [
+{
+    name: "Sydney Harbour Bridge",
+    lat: -33.851784,
+    lng: 151.210793,
+    desc: "Experience the iconic views of Sydney's harbor.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Sydney_Harbour_Bridge-16_October_2025.jpg/960px-Sydney_Harbour_Bridge-16_October_2025.jpg", 
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/20bb5380-b558-47ad-907b-8a6d948ea38b/HarbourBridge-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Bondi Beach",
+    lat: -33.891330,
+    lng: 151.276667,
+    desc: "A world-famous surf beach with a vibrant atmosphere.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Bondi_from_above.jpg/960px-Bondi_from_above.jpg",
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/f1fe19e6-f02a-490b-9a69-41981df869c0/Beach-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Royal National Park",
+    lat: -34.053873,
+    lng: 151.048522,
+    desc: "Explore the vast wilderness and coastal beauty.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Late_Afternoon_at_North_%26_South_Era.jpg/960px-Late_Afternoon_at_North_%26_South_Era.jpg",
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/38701087-7126-4a41-9f97-d4e805e14279/RoadTrip_RNP-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Sydney Motorsport Park",
+    lat: -33.803834, 
+    lng: 150.869171,
+    desc: "Sydney's premier motorsport destination.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Sydney_Motorsport_Park_Pitlane_and_Race_Control.JPG/500px-Sydney_Motorsport_Park_Pitlane_and_Race_Control.JPG",
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/fdbcc902-6e91-441f-bd9c-79dcc3af902e/Track_SMSP-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Luddenham Raceway",
+    lat: -33.8583,
+    lng: 150.7208,
+    desc: "An action-packed motorsport park featuring high-speed go-karting and a performance track.",
+    img: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/039442a3-f357-43fe-8af1-f70824eede4e/Luddenham.jpg?content-type=image%2Fjpeg", 
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/e62f0ada-212d-4cc8-ba30-cde4de0b880c/Track_Luddenham-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Vaucluse",
+    lat: -33.8559,
+    lng: 151.2772,
+    desc: "Explore historic estates, hidden harbour beaches, and stunning coastal cliff views.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Clifftop%2C_hilly_homes_in_Vaucluse_in_the_eastern_suburbs_of_Sydney%2C_Australia_b.png/960px-Clifftop%2C_hilly_homes_in_Vaucluse_in_the_eastern_suburbs_of_Sydney%2C_Australia_b.png", 
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/de061068-783a-4f43-a9ea-4648f9d4d339/VauclusePic-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "One Raceway",
+    lat: -34.8406,
+    lng: 149.6853,
+    desc: "Bi-directional motorsport circuit hosting thrilling racing events, previously known as Wakefield Park.",
+    img: "https://lh3.googleusercontent.com/gps-cs-s/APNQkAEOHN1GmxHfd5qfYk9fl5tN202gwzydmXIc-EGZ_kQ5lIzQxzlhpc84pYWTtdZhD9lGG6ZwC1aR2--M-t0VQAP_4X7G4yHHp_4-Lp0Prw03yIOEGAFkOwS4m_3OIOjrllc1W1U5xCOWjO4u=s1360-w1360-h1020-rw", 
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/2a0e1823-aec5-432b-ab3f-a9ffbaf635cd/Track_OneRaceway-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Liverpool Krispy Kreme",
+    lat: -33.9185,
+    lng: 150.9142,
+    desc: "Satisfy your sweet tooth at the home of the world's largest 'Hot Now' doughnut light.",
+    img: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/ffbe7151-abfd-4e9b-bf48-f56270d1e935/KK-liverpool.webp?content-type=image%2Fwebp", 
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/f7bbcf7e-9aad-4e84-8535-5a656a912f77/KKs-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Maroubra Beach",
+    lat: -33.9490,
+    lng: 151.2568,
+    desc: "A dedicated National Surfing Reserve renowned for its powerful waves and rich surf culture.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Maroubra_beach_Sydney_Australia.jpg/1920px-Maroubra_beach_Sydney_Australia.jpg?_=20180316135242", 
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/f1fe19e6-f02a-490b-9a69-41981df869c0/Beach-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Wollongong Beach",
+    lat: -34.4172,
+    lng: 150.9064,
+    desc: "Enjoy golden sands, scenic coastal parklands, and the iconic Flagstaff Hill Lighthouse.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Wollongong_Breakwater_Lighthouse_%2821351551761%29.jpg/500px-Wollongong_Breakwater_Lighthouse_%2821351551761%29.jpg", 
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/f1fe19e6-f02a-490b-9a69-41981df869c0/Beach-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Sydney Olympic Park",
+    lat: -33.8417,
+    lng: 151.0685,
+    desc: "A massive sporting, entertainment, and cultural hub, proud home of the 2000 Olympic Games.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Sydney_Olympic_Park_%287373562696%29.jpg/960px-Sydney_Olympic_Park_%287373562696%29.jpg", 
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/5d379141-69c9-49a5-ac2c-33479e551193/CarMeet-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Pheasant Wood Circuit",
+    lat: -34.7591,
+    lng: 149.9812,
+    desc: "A thrilling, technical motorsport track in Marulan featuring unique banked corners and driver training. Image courtesy of Auto Action.",
+    img: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/2465615f-b136-4915-b05a-4ffe3b9db5de/Lead-Pheasant-Wood-Aerial.jpg?content-type=image%2Fjpeg", 
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/6b39c695-2c4e-4197-bf01-8086f3b459d4/Track_PheasantWood-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Old Pacific Highway",
+    lat: -33.5936,
+    lng: 151.1714,
+    desc: "A legendary scenic motorcycle and driving route featuring winding twisties, lush sandstone cliffs, and classic pitstops.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Old_Pacific_Highway.jpg/1280px-Old_Pacific_Highway.jpg", 
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/38701087-7126-4a41-9f97-d4e805e14279/RoadTrip_RNP-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Sea Cliff Bridge",
+    lat: -34.25434207588753,
+    lng: 150.9733821494074,
+    desc: "A spectacular, sweeping coastal bridge that curves out over the Pacific Ocean along the Grand Pacific Drive.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/3/35/Sea_Cliff_Bridge.jpg?_=20151122234205", 
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/b795d835-898b-4c56-a6ea-fbf0b0dbcf03/GenericPhoto-Web.png?content-type=image%2Fpng" 
+},
+{
+    name: "Mount Panorama",
+    lat: -33.4475,
+    lng: 149.5583,
+    desc: "Australia's ultimate racing mecca in Bathurst, featuring a legendary public-road circuit and dramatic elevation changes.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Mount_Panorama_Pit_Straight_and_Pit_Lane_2015.JPG/960px-Mount_Panorama_Pit_Straight_and_Pit_Lane_2015.JPG", 
+    icon: "https://images.squarespace-cdn.com/content/65a72aee33244c22c79b770d/ad04d744-f0de-463e-99a2-2fc8b4f109ca/Track_Bathurst-Web.png?content-type=image%2Fpng" 
+}
+
+];
